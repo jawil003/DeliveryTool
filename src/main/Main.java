bd97578045098c7e9a7dd47d3777fc08fc0ecc3b
jawil003 <jw19980309@gmail.com>
2019-03-28 10:54:25 +0100

    added listview, scene, ... to mainGUI

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,20 @@
 package main;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import javax.swing.text.html.ListView;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-
+        Scene s = new Scene();
+        BorderPane pane = new BorderPane();
+        ListView pizzaEintraege = new ListView();
+        //TODO Complete mainView
     }
 
 
---
+++ src/main/mainStage.java
@@ -0,0 +1,12 @@
+package main;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class mainScene extends Scene {
+
+    public mainScene(Parent root) {
+        super(root);
+
+    }
+}
